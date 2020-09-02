caf9bf490a84091f9cbabd0ed67336c221f7ca3a
piyushxbajaj <piyushpiyush41@gmail.com>
2020-09-02 12:24:38 +0530

    Form validate added

--- form.js
+++ form.js
@@ -1,21 +1,41 @@
-//function validatePassword() {
-  //  var newPassword = document.getElementById('pwd').value;
-  //  var minNumberofChars = 6;
-  //  var maxNumberofChars = 16;
-  //  var regularExpression  = /[(^[a-zA-Z0-9!@#$%^&*])+?=.*[a-z]+?=.*[A-Z]+?=.*[0-9]]{6,16}$/;
-  //  alert(newPassword);
-  //  if(newPassword.length < minNumberofChars || newPassword.length > maxNumberofChars){
-  //      return alert("Wrong");
-  //  }
-  //  if(!regularExpression.test(newPassword)) {
-  //      alert("password should contain atleast one number and one special character");
-  //      return false;
-  //  }
-//}
-//validatePassword();
-  var username= document.getElementById('user_name').value;
-  var regularExpression  = /^[a-zA-Z0-9!@#$%^&*]{8,}$/;
-  if (!regularExpression.test(username))
-  {
-  alert("Username is too short");
+function validate() {
+  var usrname = document.getElementById("user_name").value;
+  var pwd = document.getElementById("pwd").value;
+  var pwd2 = document.getElementById("pwd2").value;
+  var email = document.getElementById("email").value;
+  var pass = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{16}$/;
+
+  if (usrname =="" ) {
+      alert("Please write a username");
+      return false;
   }
+  if (pwd =="" ) {
+      alert("Please write a password");
+      return false;
+  }
+  if (pwd2 =="" ) {
+      alert("Please confirm your pasword");
+      return false;
+  }
+  if (email =="" ) {
+      alert("Please write your email");
+      return false;
+  }
+  if (usrname.length <8) {
+      alert("username should be more than 8 characters");
+      return false;
+  }
+  if (pass.test(pwd) == false){
+      alert("Please enter a strong password: 16 char, 1 lowercase letter, 1 upper case letter, min 1 digit");
+      return false;
+  }
+  if (pwd!=pwd2){
+      alert("Please enter a strong password: 16 char, 1 lowercase letter, 1 upper case letter, min 1 digit");
+      return false;
+  }
+  alert("Successfully submitted");
+
+  return true;
+
+  
+}
